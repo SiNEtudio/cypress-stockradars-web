903375fe7789ba4a0097cdce7df06633ceabc68e
SiNEtudio <sinetudio@gmail.com>
2020-10-07 12:03:10 +0700

    Add viewport

--- cypress/integration/stockradars-web/access-trade-menu.spec.js
+++ cypress/integration/stockradars-web/access-trade-menu.spec.js
@@ -1,15 +1,23 @@
 /// <reference types="Cypress" />
 
-describe("Visit stockradars.co Website and access 'Trade' menu", () => {
-  beforeEach(() => {
-    cy.visit("https://stockradars.co");
-  });
+describe(
+  "Visit stockradars.co Website and access 'Trade' menu",
+  {
+    //cy.viewport(1920, 1080)
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+  },
+  () => {
+    beforeEach(() => {
+      cy.visit("https://stockradars.co");
+    });
 
-  it("Should appear \'Login\' Menu", () => {
-    cy.get('[class="nav-item"]').should("contain", "Login");
-  });
+    it("Should appear 'Login' Menu", () => {
+      cy.get('[class="nav-item"]').should("contain", "Login");
+    });
 
-  it("Should click '\Login\' Menu", () => {
-    cy.get("#navbarNavDropdown > ul > li:nth-child(7)").click();
-  });
-});
+    it("Should click 'Login' Menu", () => {
+      cy.get("#navbarNavDropdown > ul > li:nth-child(7)").click();
+    });
+  }
+);
