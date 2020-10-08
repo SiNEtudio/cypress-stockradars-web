cc641ab2a5d827611d06976674624852f539cf8b
SiNEtudio <sinetudio@gmail.com>
2020-10-08 17:53:22 +0700

    Add check menu yuanta file

--- cypress/integration/stockradars-web/check-menu-yuanta.spec.js
+++ cypress/integration/stockradars-web/check-menu-yuanta.spec.js
@@ -25,46 +25,6 @@ describe("Login with \'Yuanta Securities\'", () => {
     );
   });
 
-  it("Should alert warning text when \'username and password are empty\'", () => {
-    cy.get('[class="blue md-button md-ink-ripple flex"]')
-      .contains("เข้าสู่ระบบ")
-      .click();
-    cy.get('[class="swal2-content"]').should(
-      "contain",
-      "Please enter Username and Password"
-    );
-    cy.get('[class="swal2-confirm styled"]').contains("OK").click();
-  });
-
-  it("Should alert warning text when \'password is empty\'", () => {
-    cy.fixture("user/user-yuanta.json").then((user) => {
-      cy.get("#input_1").clear().type(user.username); // Get data from {fixturesFolder}/users/user.json
-      cy.get('[class="blue md-button md-ink-ripple flex"]')
-        .contains("เข้าสู่ระบบ")
-        .click();
-      cy.get('[class="swal2-content"]').should(
-        "contain",
-        "Please enter Username and Password"
-      );
-      cy.get('[class="swal2-confirm styled"]').contains("OK").click();
-    });
-  });
-
-  it("Should alert warning text when \'username field is empty\'", () => {
-    cy.fixture("user/user-yuanta.json").then((user) => {
-      cy.get("#input_1").clear(); //clear data in username field
-      cy.get("#input_2").clear().type(user.password); // Get data from {fixturesFolder}/users/user.json
-      cy.get('[class="blue md-button md-ink-ripple flex"]')
-        .contains("เข้าสู่ระบบ")
-        .click();
-      cy.get('[class="swal2-content"]').should(
-        "contain",
-        "Please enter Username and Password"
-      );
-      cy.get('[class="swal2-confirm styled"]').contains("OK").click();
-    });
-  });
-
   it("Should login when \'username and password are correct\'", () => {
     cy.fixture("user/user-yuanta.json").then((user) => {
       cy.get("#input_1").clear().type(user.username); // Get data from {fixturesFolder}/users/user.json
