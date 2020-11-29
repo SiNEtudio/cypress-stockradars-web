89f8c36a199263c2e969b4a5a0ccc65e3f3483a4
SiNEtudio <sinetudio@gmail.com>
2020-11-29 20:55:58 +0700

    Add test case check chart TradingView

---
+++ cypress/integration/stockradars-web/check-data-chart.spec.js
@@ -0,0 +1,62 @@
+/// <reference types="Cypress" />
+
+describe("Check data in 'Chart' menu", () => {
+  // beforeEach(() => {
+  it("Should visit StockRadars Website (Broker Login)", () => {
+    cy.visit("/trade/");
+  });
+
+  it("Should appear select broker dropdown", () => {
+    cy.get('[class="blue md-button md-ink-ripple flex"]').should(
+      "contain",
+      "เลือกบริษัทหลักทรัพย์ที่นี่"
+    );
+  });
+
+  it("Should select 'Yuanta Securities' broker", () => {
+    cy.get('[class="blue md-button md-ink-ripple flex"]')
+      .contains("เลือกบริษัทหลักทรัพย์ที่นี่")
+      .click();
+    cy.get('[class="_md"]').contains("YUANTA").click();
+    cy.get('[class="img-broker img-responsive"]').should(
+      "have.attr",
+      "src",
+      "https://storage-th-xbkk.stockradars.co/images/broker/YUANTA.png"
+    );
+  });
+
+  it("Should login when 'username and password are correct'", () => {
+    cy.fixture("user/user-yuanta.json").then((user) => {
+      cy.get("#input_1").clear().type(user.username); // Get data from {fixturesFolder}/users/user.json
+      cy.get("#input_2").clear().type(user.password); // Get data from {fixturesFolder}/users/user.json
+      cy.get('[class="blue md-button md-ink-ripple flex"]')
+        .contains("เข้าสู่ระบบ")
+        .click();
+      cy.intercept({
+        method: "POST",
+        url: "https://realtime.stockradars.co/setmds/pull",
+      }).as("getPull");
+      cy.wait("@getPull", { timeout: 15000 });
+
+      cy.get(".setindex-bar").should("contain", "SET");
+    });
+  });
+
+  it("Should access 'Chart' menu", () => {
+    cy.wait(21000);
+    cy.get('[class="md-list-item-text"]').contains("Chart").click();
+    cy.url().should("eq", "https://stockradars.co/trade/#/chart");
+  });
+
+  it('Should display TradingView', () => {
+    cy.get('.symbol-edit-inputspacer').should("contain","SET");
+    
+  });
+
+  // it("Should logout to landing page", () => {
+  //   cy.get('[class="md-list-item-text"]').contains("Logout").click();
+  //   cy.get('[class="_md md-default-theme md-transition-in"]')
+  //     .contains("YES")
+  //     .click();
+  // });
+});
